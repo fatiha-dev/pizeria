0173f28bdc133d120c839a2a9957a518d3aec718
fatiha-dev <tihaelbou@gmail.com>
2023-07-21 00:35:03 +0100

    edit orders link page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
-import cover from '../assets/pizza2.jpg';
+import cover from '../assets/pizza1.jpg';
 import './page.css'
 import { Footer } from '../components/Footer';
+import { useNavigate } from "react-router-dom";
 
 export function Home() {
+    const navigate = useNavigate();
     return(
         <div className="home">
             <img src={cover}/>
-            <button className='order-btn fs-1'>Order a pizza</button>
-            <button className='recipes-btn fs-2'>Recipes</button>
-            <button className='about-btn fs-3'>Who we are?</button>
+            <button onClick={() => navigate("/orders")} className='order-btn fs-1'>Order a pizza</button>
+            <button onClick={() => navigate("/recipes")} className='recipes-btn fs-2'>Recipes</button>
+            <button onClick={() => navigate("/about")} className='about-btn fs-3'>Who we are?</button>
             <Footer />
         </div>
-
     )
 }
