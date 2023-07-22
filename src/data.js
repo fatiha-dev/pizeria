21e9a352c8adba62cbd782ee0a88bc46ceb99a15
fatiha-dev <tihaelbou@gmail.com>
2023-07-22 15:39:08 +0100

    add data.js file

---
+++ src/data.js
@@ -0,0 +1,72 @@
+import pizza0 from "./assets/pizza0.jpeg"
+import pizza1 from "./assets/pizza1.jpg"
+import pizza3 from "./assets/pizza3.jpg"
+import pizza4 from "./assets/pizza4.jpg"
+import pizza2 from "./assets/pizza2.jpg"
+import pizza5 from "./assets/pizza5.jpg"
+export const recipes = [
+   {
+        id:1,
+        img : pizza0,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:2,
+        img : pizza1,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:3,
+        img : pizza3,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:4,
+        img : pizza4,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:5,
+        img : pizza1,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:6,
+        img : pizza3,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:7,
+        img : pizza5,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:8,
+        img : pizza2,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   {
+        id:9,
+        img : pizza1,
+        pizzaName: "Pizza",
+        price: 20,
+        ingredients: "Tomato sause, cheese, motzarella, black olive, meat balls"
+   },
+   
+]
